7cff4437720804b0dcffd2fdfb44d8317c745c8f
thief01 <pst12345@wp.pl>
2025-10-06 01:50:36 +0200

    fix pareting

--- Assets/_Project/Scripts/World/MovingPlatform.cs
+++ Assets/_Project/Scripts/World/MovingPlatform.cs
@@ -20,11 +20,18 @@ namespace _Project.Scripts.World
             previousPosition = transform.position;
         }
 
-        private void OnTriggerStay(Collider other)
+        private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
                 return;
-            other.transform.position = target.position + moveOffset;
+            other.transform.parent = target;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
+                return;
+            other.transform.parent = null;
         }
     }
 }
