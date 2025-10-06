1c86ff6ef37f9d9ceaa76f54adbae0e2b6d7ca31
thief01 <pst12345@wp.pl>
2025-10-06 03:18:40 +0200

    rm comment

--- Assets/_Project/Scripts/World/MovingPlatform.cs
+++ Assets/_Project/Scripts/World/MovingPlatform.cs
@@ -6,26 +6,6 @@ namespace _Project.Scripts.World
     public class MovingPlatform : MonoBehaviour
     {
         [SerializeField] private Transform target;
-        
-        private Vector3 previousPosition;
-        private Vector3 moveOffset = Vector3.zero;
-
-        // private void Update()
-        // {
-        //     moveOffset = previousPosition - target.position;
-        // }
-        //
-        // private void LateUpdate()
-        // {
-        //     previousPosition = transform.position;
-        // }
-        //
-        // private void OnTriggerStay(Collider other)
-        // {
-        //     if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
-        //         return;
-        //     other.transform.position = target.position + moveOffset;
-        // }
 
         private void OnTriggerEnter(Collider other)
         {
