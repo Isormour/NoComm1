3855094d0a2b77630fc3df02185cc6724a383fb5
thief01 <pst12345@wp.pl>
2025-10-06 02:07:48 +0200

    fix moving platform

--- Assets/_Project/Scripts/World/MovingPlatform.cs
+++ Assets/_Project/Scripts/World/MovingPlatform.cs
@@ -10,35 +10,35 @@ namespace _Project.Scripts.World
         private Vector3 previousPosition;
         private Vector3 moveOffset = Vector3.zero;
 
-        private void Update()
-        {
-            moveOffset = previousPosition - target.position;
-        }
+        // private void Update()
+        // {
+        //     moveOffset = previousPosition - target.position;
+        // }
+        //
+        // private void LateUpdate()
+        // {
+        //     previousPosition = transform.position;
+        // }
+        //
+        // private void OnTriggerStay(Collider other)
+        // {
+        //     if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
+        //         return;
+        //     other.transform.position = target.position + moveOffset;
+        // }
 
-        private void LateUpdate()
-        {
-            previousPosition = transform.position;
-        }
-
-        private void OnTriggerStay(Collider other)
+        private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
                 return;
-            other.transform.position = target.position + moveOffset;
+            other.transform.parent = target;
         }
-
-        // private void OnTriggerEnter(Collider other)
-        // {
-        //     if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
-        //         return;
-        //     other.transform.parent = target;
-        // }
-        //
-        // private void OnTriggerExit(Collider other)
-        // {
-        //     if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
-        //         return;
-        //     other.transform.parent = null;
-        // }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
+                return;
+            other.transform.parent = null;
+        }
     }
 }
